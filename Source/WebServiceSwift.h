da218ce808ea8f628c8662515263e124c9e76fe1
Vitaliy Korotkiy <vir@provir.ru>
2017-08-27 16:00:13 +1000

    Update pod

--- Source/WebServiceSwift.h
+++ Source/WebServiceSwift.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 ProVir. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for WebServiceSwift.
 FOUNDATION_EXPORT double WebServiceSwiftVersionNumber;
